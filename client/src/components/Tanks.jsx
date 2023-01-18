c10fbbdb8c8d95c1ba1fce943aac5bd7f25d1f6d
Jesus Rivera <r.jesus9107@gmail.com>
2023-01-17 20:00:06 -0800

    added zoom button

--- client/src/components/Tanks.jsx
+++ client/src/components/Tanks.jsx
@@ -5,14 +5,23 @@ const Tanks = () => {
   const [tankSelected, setTankSelected] = useState(false)
   const [tanks, setTanks] = useState([{ id: 1 }, { id: 2 }, { id: 3 }])
 
-  const selectTank = id => {
+  const zoomIn = id => {
     let tank = document.getElementById(`${id}`)
+    tank.classList.add(`tank${id}ZoomedIn`)
+    tank.style.zIndex = 10
+    setTankSelected(true)
+  }
 
-    tank.style.position = "relative"
-  
-    id == 1 ? tank.style.transform = 'scale(2) translate(50%, -50%)' :
-    id == 2 ? tank.style.transform = 'scale(2) translateY(-50%)' :
-    tank.style.transform = 'scale(2) translate(-50%, -50%)' 
+  const zoomOut = id => {
+    let tank = document.getElementById(`${id}`)
+    tank.classList.remove(`tank${id}ZoomedIn`)
+    tank.style.zIndex = 0
+    setTankSelected(false)
+  }
+
+  const triggerZoom = id => {
+    if (tankSelected) zoomOut(id)
+    else zoomIn(id)
   }
 
   return (
@@ -21,7 +30,12 @@ const Tanks = () => {
         <div className="big-tank"></div>
       </div>
         <div className="small-tank-container">
-          {tanks.map(tank => <div className='tank' id={`${tank.id}`} onClick={() => selectTank(`${tank.id}`)}></div>)}
+          {tanks.map(tank => {
+            return (
+              <div key={tank.id} className='tank' id={`${tank.id}`}>
+                <div className='zoomTrigger' onClick={() => triggerZoom(`${tank.id}`)}></div>
+              </div>
+            )})}
         </div>
     </>
   )
