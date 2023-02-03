d564597a102a8a2be83b1ec2b34468d8e13a7558
Jesus Rivera <r.jesus9107@gmail.com>
2023-02-03 10:35:38 -0800

    db accesss configurations

--- server/services/db.js
+++ server/services/db.js
@@ -1,9 +1,12 @@
+const dotenv = require('dotenv')
+dotenv.config()
+
 const mysql = require('mysql2')
 const conn = mysql.createConnection({
-  host: '127.0.0.1',
-  user: 'root',
-  password: "root",
-  database: "mgp2"
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE
 })
 
 conn.connect()
